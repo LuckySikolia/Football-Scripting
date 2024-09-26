ed51032ca4c62812e70dbdcf5cf5715d3b45a390
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2024-09-26 19:24:13 +0300

    Fix enemy spawn not increasing
    -changed enemy spawn trigger from powerup to enemy count on field
    - Fix enemy spawn count increase counter
    - fix collision direction of player, ensure on collision enemy moves away

--- Assets/Challenge 4/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -20,13 +20,13 @@ public class SpawnManagerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        enemyCount = GameObject.FindGameObjectsWithTag("Powerup").Length;
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
 
         if (enemyCount == 0)
         {
             SpawnEnemyWave(waveCount);
         }
-
+        Debug.Log("Respawn hen enemy count = 0");
     }
 
     // Generate random spawn position for powerups and enemy balls
@@ -49,12 +49,13 @@ public class SpawnManagerX : MonoBehaviour
         }
 
         // Spawn number of enemy balls based on wave number
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < enemiesToSpawn; i++)
         {
             Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
         }
 
-        waveCount++;
+        waveCount++;        
+        Debug.Log("wave coint increases by 1");
         ResetPlayerPosition(); // put player back at start
 
     }
